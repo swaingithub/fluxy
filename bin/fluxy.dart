cd32b176bacdc68bcae0929fdf123d974310582e
swaingithub <swainrakesh990@gmail.com>
2026-02-13 21:19:33 +0530

    Add initial Fluxy CLI with commands for project initialization, running, building, doctor checks, deployment, and cloud management.

--- bin/fluxy.dart
+++ bin/fluxy.dart
@@ -4,7 +4,7 @@ import 'package:path/path.dart' as p;
 
 import 'package:fluxy/src/cli/cloud.dart';
 
-const String version = '0.1.2';
+const String version = '0.1.3';
 
 void main(List<String> arguments) async {
   final parser = ArgParser()
